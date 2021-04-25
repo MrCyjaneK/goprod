7e003b0c63baf0e75f760f337a717aa44b71eb2f
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-04-25 12:10:55 +0200

    Updated versioning

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"os"
 	"os/user"
+	"strconv"
 	"strings"
+	"time"
 
 	"git.mrcyjanek.net/mrcyjanek/goprod/compiler"
 	"git.mrcyjanek.net/mrcyjanek/goprod/debpackage"
@@ -16,13 +18,25 @@ var (
 	builddir = flag.String("builddir", "build", "Where should the files get saved.")
 	binname  = flag.String("binname", "helloworld", "What is the program name?")
 	tags     = flag.String("tags", "goprod", "Tags that are passed to go build command")
-	version  = flag.String("version", "0.0.0", "Version of your program.")
+	versiona = flag.String("version", "0.0.0", "Version of your program.")
 	ndka     = flag.String("ndk", "~/Android/Sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/linux-x86_64/bin/", "Path to android toolchain")
 )
 var ndk string
+var version string
 
 func main() {
-	flag.Parse()
+	t := time.Now()
+	year := "" + strconv.Itoa(t.Year())
+	month := "0" + strconv.Itoa(int(t.Month()))
+	month = month[len(month)-2:]
+	day := "0" + strconv.Itoa(t.Day())
+	day = day[len(day)-2:]
+	hour := "0" + strconv.Itoa(t.Hour())
+	hour = hour[len(hour)-2:]
+	minute := "0" + strconv.Itoa(t.Minute())
+	minute = minute[len(minute)-2:]
+	version = *versiona + "-" + year + month + day + hour + minute
+
 	usr, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
@@ -40,9 +54,8 @@ func main() {
 		log.Println("Compiling...")
 		compiler.Build(i, *tags, *binname, *builddir+"/bin", ndk)
 		if GOOS == "linux" {
-
 			log.Println("Packaging...")
-			debpackage.Build(i, *binname, *builddir+"/bin", *builddir+"/deb", *version)
+			debpackage.Build(i, *binname, *builddir+"/bin", *builddir+"/deb", version)
 		}
 	}
 }
