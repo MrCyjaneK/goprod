84a031df86beae28df6afc6aa7ff351930480b6c
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-04-27 02:03:10 +0200

    make packaging optional

--- main.go
+++ main.go
@@ -14,13 +14,14 @@ import (
 )
 
 var (
-	combo    = flag.String("combo", "linux/arm;linux/386;linux/arm64;linux/amd64", "Combo that I should serve")
-	builddir = flag.String("builddir", "build", "Where should the files get saved.")
-	ldflags  = flag.String("ldflags", "", "Things to get passwd by with --ldflags")
-	binname  = flag.String("binname", "helloworld", "What is the program name?")
-	tags     = flag.String("tags", "goprod", "Tags that are passed to go build command")
-	versiona = flag.String("version", "0.0.1", "Version of your program.")
-	ndka     = flag.String("ndk", "~/Android/Sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/linux-x86_64/bin/", "Path to android toolchain")
+	combo     = flag.String("combo", "linux/arm;linux/386;linux/arm64;linux/amd64", "Combo that I should serve")
+	builddir  = flag.String("builddir", "build", "Where should the files get saved.")
+	ldflags   = flag.String("ldflags", "", "Things to get passwd by with --ldflags")
+	binname   = flag.String("binname", "helloworld", "What is the program name?")
+	tags      = flag.String("tags", "goprod", "Tags that are passed to go build command")
+	versiona  = flag.String("version", "0.0.1", "Version of your program.")
+	ndka      = flag.String("ndk", "~/Android/Sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/linux-x86_64/bin/", "Path to android toolchain")
+	shouldpkg = flag.Bool("package", true, "Should we create a package out of the binary?")
 )
 var ndk string
 var version string
@@ -55,7 +56,7 @@ func main() {
 		GOOS := spl[0]
 		log.Println("Compiling...")
 		compiler.Build(i, *tags, *binname, *builddir+"/bin", ndk, *ldflags)
-		if GOOS == "linux" {
+		if GOOS == "linux" && *shouldpkg {
 			log.Println("Packaging...")
 			debpackage.Build(i, *binname, *builddir+"/bin", *builddir+"/deb", version)
 		}
