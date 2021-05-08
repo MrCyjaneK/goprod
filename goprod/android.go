374ef45487c7d4dedb46bd3cc0dbbfe3d06760d1
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-05-08 09:08:48 +0200

    added android tools

---
+++ goprod/android.go
@@ -0,0 +1,30 @@
+package goprod
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Intent struct {
+	Type string `json:"type"`
+	Data struct {
+		URI     string `json:"uri"`
+		Package string `json:"package"`
+		Extra   []struct {
+			Key   string `json:"key"`
+			Value string `json:"value"`
+		} `json:"extra"`
+	} `json:"data"`
+}
+
+func CallIntent(i Intent) error {
+	b, err := json.Marshal(i)
+	if err != nil {
+		log.Println("goporod/android.go CallIntent()", err)
+		return err
+	}
+	fmt.Println(string(b))
+	fmt.Println("goprod:" + string(b))
+	return nil
+}
